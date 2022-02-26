f64ca5deac7d947d00d8162dc770ff1f56b90a65
Koreen Legaspi <36668830+koreenml@users.noreply.github.com>
2022-02-26 14:55:24 -0500

    implemented video recording

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet ,Text, View, Button, Image} from 'react-native';
 import { Camera } from 'expo-camera';
+import { Video } from 'expo-av';
 
 export default function App() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  // audio/video permissions
+  const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  const [record, setRecord] = useState(null);
+  // video playback
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
 useEffect(() => {
     (async () => {
       const cameraStatus = await Camera.requestPermissionsAsync();
       setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+      setHasAudioPermission(audioStatus.status === 'granted');
+
 })();
   }, []);
+  
 const takePicture = async () => {
     if(camera){
         const data = await camera.takePictureAsync(null)
@@ -20,9 +33,24 @@ const takePicture = async () => {
     }
   }
 
-  if (hasCameraPermission === false) {
+  if (hasCameraPermission === false || hasAudioPermission === false || hasCameraPermission === null || hasAudioPermission === null) {
     return <Text>No access to camera</Text>;
   }
+
+  const takeVideo = async () => {
+    if(camera){
+        const data = await camera.recordAsync()
+        setRecord(data.uri);
+        console.log(data.uri);
+    }
+  }
+
+
+  const stopVideo = async () => {
+    camera.stopRecording();
+  }
+
+
   return (
    <View style={{ flex: 1}}>
       <View style={styles.cameraContainer}>
@@ -32,19 +60,65 @@ const takePicture = async () => {
             type={type}
             ratio={'1:1'} />
       </View>
+      <Button
+        title="Flip Video"
+        onPress={() => {
+        setType(
+          type === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back
+          );
+          }}>
+      </Button>
 
-       <Button title="Take Picture" onPress={() => takePicture()} />
-        {image && <Image source={{uri: image}} style={{flex:1}}/>}
+      <Button title="Take video" onPress={() => takeVideo()} />
+      <Button title="Stop Video" onPress={() => stopVideo()} />
+
+       {/* <Button title="Take Picture" onPress={() => takePicture()} />
+        {image && <Image source={{uri: image}} style={{flex:1}}/>} */}
+
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: record,
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+
    </View>
   );
 }
+
 const styles = StyleSheet.create({
   cameraContainer: {
       flex: 1,
-    //   flexDirection: 'row'
+      flexDirection: 'row'
   },
   fixedRatio:{
       flex: 1,
       aspectRatio: 1
-  }
+  },
+  video: {
+    alignSelf: 'center',
+    width: 350,
+    height: 220,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
