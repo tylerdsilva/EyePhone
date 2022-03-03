bc89b0521726b96bc650b743f7549f0af78d5337
m3zhen <35463824+m3zhen@users.noreply.github.com>
2022-03-02 22:17:50 -0500

    Add files via upload

---
+++ filter scripts/testScript.m
@@ -0,0 +1,59 @@
+RGB = imread('P4.jpg');
+
+if ndims(RGB) == 3
+    I = rgb2gray(RGB);   
+else
+    I = RGB;
+end
+
+BW = imbinarize(I,'adaptive','ForegroundPolarity','dark','Sensitivity',0);
+BW2 = imcomplement(BW);
+
+figure
+imshowpair(BW2,RGB,'montage')
+title('Binary Version of Image')
+hold on
+
+L = bwlabel(BW2);
+
+vislabels(L)
+hold on
+
+segmentSizes = regionprops(L,'Area');
+segments = [segmentSizes.Area];
+filteredSegments = find(segments > 800);
+
+%filter out segments < 900 pixels
+L(~ismember(L, filteredSegments)) = 0;
+
+%display labelled segments
+%vislabels(L)
+%hold on
+
+%calculate region properties and analyze validity of segments
+figure
+imshow(L)
+hold on
+
+regionProperties = regionprops(L, 'Circularity', 'Solidity', 'MinorAxisLength', 'MajorAxisLength', 'BoundingBox');
+
+isValid = false;
+for segmentIndex = filteredSegments
+    circularity = regionProperties(segmentIndex).Circularity; 
+    solidity = regionProperties(segmentIndex).Solidity; 
+    minorAxisLength = regionProperties(segmentIndex).MinorAxisLength; 
+    majorAxisLength = regionProperties(segmentIndex).MajorAxisLength; 
+    boundingBox = regionProperties(segmentIndex).BoundingBox; 
+
+    rectangle('Position', [boundingBox(1),boundingBox(2),boundingBox(3),boundingBox(4)],'EdgeColor','r','LineWidth',2);
+    hold on
+
+    aspectRatio = min(minorAxisLength, majorAxisLength) / max(minorAxisLength, majorAxisLength);
+    if(circularity > 0.15 && solidity > 0.5 && aspectRatio > 0.5)
+        %disp(segmentIndex);
+        isValid = true;
+        %close all
+        break
+    end
+end
+%close all
